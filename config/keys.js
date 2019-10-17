127ab131953a5993c4f02541b46525b2b85bf5a2
Abinash Sahu <abinash.sahu@codeclouds.in>
2019-10-17 12:18:39 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports = {
+        mongoURI: "mongodb://nandymandy123:nandymandy123@ds129906.mlab.com:29906/mevn_auth",
+        secret: 'yoursecret'
+    };
+} else {
+    module.exports = {
+        mongoURI: "mongodb://localhost:27017/dtr",
+        secret: 'yoursecret'
+    };
+}
